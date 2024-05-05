3015f8396bbcc2dec741d2ca0a9b2a17d97fe502
Tiny_Murky <murky0830@gmail.com>
2024-05-05 18:12:08 +0800

    audit report service, Checkpoint

--- src/common/interfaces/audit_report.ts
+++ src/common/interfaces/audit_report.ts
@@ -1,3 +1,4 @@
+import { LifeCycleType } from '../types/audit_report';
 import { BalanceSheet, isBalanceSheet } from './balance_sheet';
 import { CashFlow, isCashFlow } from './cash_flow';
 import {
@@ -9,6 +10,28 @@ export interface FinancialStatements {
   balanceSheet: BalanceSheet;
   comprehensiveIncome: ComprehensiveIncome;
   cashFlow: CashFlow;
+}
+
+export interface AuditReport {
+  balanceSheet: {
+    balanceSheet: BalanceSheet;
+    balanceSheetRatios: { [key: string]: number };
+    balanceSheetAnalysis: string;
+  };
+  comprehensiveIncome: {
+    comprehensiveIncome: ComprehensiveIncome;
+    comprehensiveIncomeRatios: { [key: string]: number };
+    comprehensiveIncomeAnalysis: string;
+  };
+  cashFlow: {
+    cashFlow: CashFlow;
+    cashFlowRatios: { [key: string]: number };
+    cashFlowAnalysis: string;
+  };
+  lifeCycle: LifeCycleType;
+  creditRating: string;
+  financialStatementsAnalysis: string;
+  summary: string;
 }
 
 //Info Murky (20240505): type guards
