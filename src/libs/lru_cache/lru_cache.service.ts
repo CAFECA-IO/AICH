68bf94e89379073950650a0f0c1146f2c32c8e5f
Tiny_Murky <murky0830@gmail.com>
2024-05-29 13:24:44 +0800

    complete add special id to lru_cache

--- src/libs/lru_cache/lru_cache.service.ts
+++ src/libs/lru_cache/lru_cache.service.ts
@@ -5,43 +5,66 @@ import { PROGRESS_STATUS } from '@/constants/common';
 import { LruServiceOptions } from '@/interfaces/lru';
 import { LRU_CACHE_CONFIG } from '@/constants/configs/config';
 
+// Info Murky (20240422) 儲存在LRU Cache的key僅為有 _idLength 長度的hash值
+// 但是為了讓每次傳出去的key有所不同，回傳的key值要有原始的key值 + 依照timestamp hash過的值
+// 並在取得值時，先檢查是否為hash過的key值，如果是則直接取值，如果不是則先hash後再取值，並且要truncate到_idLength長度
+
 @Injectable()
 export class LruCacheService<T> {
-  private cache: Map<string, LRUNode<T>>;
+  private _cache: Map<string, LRUNode<T>>;
 
-  private most: LRUNode<T>;
+  private _most: LRUNode<T>;
 
-  private least: LRUNode<T>;
+  private _least: LRUNode<T>;
 
-  private capacity: number = LRU_CACHE_CONFIG.DEFAULT_CAPACITY;
+  private _capacity: number = LRU_CACHE_CONFIG.DEFAULT_CAPACITY;
 
-  private idLength: number = LRU_CACHE_CONFIG.DEFAULT_ID_LENGTH;
+  private _idLength: number = LRU_CACHE_CONFIG.DEFAULT_ID_LENGTH;
 
   constructor(
     @Inject('LRU_SERVICE_OPTIONS') { capacity, idLength }: LruServiceOptions,
   ) {
-    this.capacity = capacity;
-    this.idLength = idLength;
-    this.cache = new Map<string, LRUNode<T>>();
-    this.most = new LRUNode<T>('', PROGRESS_STATUS.IN_PROGRESS, null);
-    this.least = new LRUNode<T>('', PROGRESS_STATUS.IN_PROGRESS, null);
-    this.least.next = this.most;
-    this.most.prev = this.least;
+    this._capacity = capacity;
+    this._idLength = idLength;
+    this._cache = new Map<string, LRUNode<T>>();
+    this._most = new LRUNode<T>('', PROGRESS_STATUS.IN_PROGRESS, null);
+    this._least = new LRUNode<T>('', PROGRESS_STATUS.IN_PROGRESS, null);
+    this._least.next = this._most;
+    this._most.prev = this._least;
   }
 
   public hashId(inputString: string): string {
     const hash = createHash('sha256').update(inputString).digest('hex');
-    return hash.substring(0, this.idLength);
+    return hash.substring(0, this._idLength);
   }
 
-  private isHashId(key: string): boolean {
-    const regex = `^[0-9a-f]{${this.idLength}}$`;
+  private _generateTimestampHash(): string {
+    const timestamp = new Date().getTime().toString();
+    return this.hashId(timestamp);
+  }
+
+  private _combineIdWithTimestampHash(hashId: string): string {
+    const timestampHash = this._generateTimestampHash();
+    return hashId + timestampHash;
+  }
+
+  private _extractOriginalId(combinedId: string) {
+    return combinedId.slice(0, this._idLength);
+  }
+
+  private _isHashId(key: string): boolean {
+    const regex = `^[0-9a-f]{${this._idLength}}$`;
+    return new RegExp(regex).test(key);
+  }
+
+  private _isCombinedId(key: string): boolean {
+    const regex = `^[0-9a-f]{${this._idLength}}[0-9a-f]{${this._idLength}}$`;
     return new RegExp(regex).test(key);
   }
 
   // Info Murky (20240422) This method I want to keep with object not static
   // eslint-disable-next-line class-methods-use-this
-  private remove(node: LRUNode<T>): LRUNode<T> {
+  private _removeNode(node: LRUNode<T>): LRUNode<T> {
     // Info Murky (20240422) !. means non-null assertion operator
     node.prev!.next = node.next;
     node.next!.prev = node.prev;
@@ -50,7 +73,7 @@ export class LruCacheService<T> {
 
   // Info Murky (20240422) This method I want to keep with object not static
   // eslint-disable-next-line class-methods-use-this
-  private insert(node: LRUNode<T>, target: LRUNode<T>): void {
+  private _insertNode(node: LRUNode<T>, target: LRUNode<T>): void {
     node.next = target;
     node.prev = target.prev;
     target.prev!.next = node;
@@ -59,19 +82,23 @@ export class LruCacheService<T> {
 
   public get(key: string): { status: PROGRESS_STATUS; value: T | null } {
     // Info Murky (20240423) key need to be hashed already
-    if (!this.isHashId(key)) {
+    if (!this._isHashId(key)) {
       key = this.hashId(key);
     }
 
-    if (!this.cache.has(key)) {
+    if (this._isCombinedId(key)) {
+      key = this._extractOriginalId(key);
+    }
+
+    if (!this._cache.has(key)) {
       return {
         status: PROGRESS_STATUS.NOT_FOUND,
         value: null,
       };
     }
 
-    const node = this.remove(this.cache.get(key)!);
-    this.insert(node, this.most);
+    const node = this._removeNode(this._cache.get(key)!);
+    this._insertNode(node, this._most);
     return {
       status: node.status,
       value: node.value,
@@ -79,20 +106,24 @@ export class LruCacheService<T> {
   }
 
   public put(key: string, status: PROGRESS_STATUS, value: T | null): string {
-    if (!this.isHashId(key)) {
+    if (!this._isHashId(key)) {
       key = this.hashId(key);
     }
     const newNode = new LRUNode<T>(key, status, value);
 
-    if (this.cache.has(key)) {
-      this.remove(this.cache.get(key)!);
+    if (this._cache.has(key)) {
+      this._removeNode(this._cache.get(key)!);
     }
-    this.cache.set(key, newNode);
-    this.insert(newNode, this.most);
-    if (this.cache.size > this.capacity) {
-      const leastNode = this.least.next!;
-      this.remove(leastNode);
-      this.cache.delete(leastNode.key);
+    this._cache.set(key, newNode);
+    this._insertNode(newNode, this._most);
+    if (this._cache.size > this._capacity) {
+      const leastNode = this._least.next!;
+      this._removeNode(leastNode);
+      this._cache.delete(leastNode.key);
+    }
+
+    if (!this._isCombinedId(key)) {
+      key = this._combineIdWithTimestampHash(key);
     }
 
     return key;
