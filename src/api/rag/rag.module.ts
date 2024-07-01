52a72d4f026fe560e2c57eba8bd0f3a303f54720
RexBearIU <rexbear87941@gmail.com>
2024-07-01 09:43:25 +0800

    feat:add version

--- src/api/rag/rag.module.ts
+++ src/api/rag/rag.module.ts
@@ -1,6 +1,6 @@
 // rag.module.ts
 import { Module } from '@nestjs/common';
-import { RagService } from './rag.service';
+import { RagService } from '@/api/rag/rag.service';
 import { QdrantModule } from '@/api/qdrant/qdrant.module';
 import { OllamaModule } from '@/api/ollama/ollama.module';
 import { RagController } from '@/api/rag/rag.controller';
