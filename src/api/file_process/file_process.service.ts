bbc9549de0d4c3c7bc67dcb738971cccd37fbc64
RexBearIU <rexbear87941@gmail.com>
2024-08-12 11:18:26 +0800

    fix:type annotation error

--- src/api/file_process/file_process.service.ts
+++ src/api/file_process/file_process.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { Document } from 'langchain/document';
 
 @Injectable()
 export class FileProcessService {
-  async processPDFFile(filePath: string) {
+  async processPDFFile(filePath: string): Promise<Document[]> {
     const loader = new PDFLoader(filePath);
     const docs = await loader.load();
     const splitter = new RecursiveCharacterTextSplitter();
-    return splitter.splitDocuments(docs);
+    const processedDocs = await splitter.splitDocuments(docs);
+    return processedDocs;
   }
 }
