936d449437e8805c68bd2a17893edb4d4fe82160
RexBearIU <rexbear87941@gmail.com>
2024-08-14 15:10:51 +0800

    feat: add chat api

--- README.md
+++ README.md
@@ -40,16 +40,21 @@ $ npm install
 1. Create a `.env` file in the root directory of the project.
 2. Start the qdrant server. [Qdrant](https://github.com/qdrant/qdrant)
 3. Start the ollama server. [Ollama](https://github.com/ollama/ollama)
-4. Donwload the chat-model, embedding model you want in ollama. 
-5. Create the google project and get the Google Vision API credential and add it to the `.env` file as `GOOGLE_PROJECT_ID` and `GOOGLE_CREDENTIALS_BASE64`.
-6. Final `.env` file should look like this:
+4. Donwload the needed models
+
+```bash
+ollama pull llama3.1
+ollama pull nomic-embed-text
+```
+1. Create the google project and get the Google Vision API credential and add it to the `.env` file as `GOOGLE_PROJECT_ID` and `GOOGLE_CREDENTIALS_BASE64`.
+2. Final `.env` file should look like this:
 ```
 GOOGLE_PROJECT_ID=your-google-project-id
 GOOGLE_CREDENTIALS_BASE64=your-google-credentials-base64
 OLLAMA_HOST=http://localhost:11434
-REPORT_MODEL=your-report-model
-EMBEDDING_MODEL=your-embedding-model
-CHAT_MODEL=your-chat-model
+REPORT_MODEL=llama3.1
+EMBEDDING_MODEL=nomic-embed-text
+CHAT_MODEL=llama3.1
 QDRANT_HOST=http://localhost:6333
 QDRANT_COLLECTION_NAME=your-collection-name
 ```
--- src/libs/utils/stream.ts
+++ src/libs/utils/stream.ts
@@ -1,29 +1,32 @@
-import { StreamableFile } from '@nestjs/common';
+import { Logger, StreamableFile } from '@nestjs/common';
 import { PassThrough } from 'stream';
 
-export async function processChatStream(stream) {
-  const processedStream = new PassThrough(); // 使用 PassThrough 來處理和轉發數據
+export async function processChatStream(
+  stream: ReadableStream,
+): Promise<StreamableFile> {
+  const processedStream = new PassThrough();
+  const logger = new Logger('processChatStream');
 
+  // 异步处理数据流
   (async () => {
-    for await (const chunk of stream) {
-      try {
-        // 假设 chunk 是一个包含 answer 属性的对象
-        if (chunk.answer) {
-          const canContinue = processedStream.write(chunk.answer);
+    try {
+      for await (const chunk of stream) {
+        const data = chunk.answer || chunk.content;
+        if (data) {
+          const canContinue = processedStream.write(data);
           if (!canContinue) {
             await new Promise((resolve) =>
               processedStream.once('drain', resolve),
             );
           }
         }
-      } catch (error) {
-        this.logger.warn(`Error in chatWithHistory: ${error}`);
-        processedStream.end();
-        throw error; // 或处理错误
       }
+      processedStream.end(); // 完成写入，关闭流
+    } catch (error) {
+      logger.error(`Error in processing stream: ${error}`);
+      processedStream.destroy(error); // 销毁流并传递错误
     }
-    processedStream.end();
   })();
-  const streamableFile = new StreamableFile(processedStream);
-  return streamableFile;
+
+  return new StreamableFile(processedStream);
 }
