b4fea9b5663302d68fb8baf8c405a5cd66e93b63
Luphia Chang <luphia@cafeca.io>
2024-11-26 19:14:59 +0800

    folder initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { promises as fs } from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { RequestMethod, VersioningType } from '@nestjs/common';
 import configration from '@/constants/configs/configration';
+
+async function prepare(): Promise<void> {
+  /* Info: (20241126 - Luphia) Prepare the environment before starting the application.
+   * 1. Prepare application folder ${BASE_STORAGE_PATH}
+   */
+  // Info: (20241126 - Luphia) from .env DATABASE_URL
+  const appFolder = process.env.BASE_STORAGE_PATH;
+
+  // Info: (20241126 - Luphia) use fs promisify to create folder
+  try {
+    await fs.mkdir(appFolder, { recursive: true });
+  } catch (e) {
+    // Info: (20241126 - Luphia) if error, prepare to die
+  }
+}
+
 async function bootstrap() {
+  await prepare();
   const port = configration().port;
   const app = await NestFactory.create(AppModule, {
     // Info Murky (20240429): Set the logger level for the application.
